85e9c4876f1a8d2a3276e4a88c2ec49e6a7992b5
Tobias Hesterberg <tobiashesterberg3103@gmail.com>
2025-04-26 17:30:46 +0200

    Added feature to let the peer register itself to the server via webSocket.

--- src/app/websocketComponent.tsx
+++ src/app/websocketComponent.tsx
@@ -1,20 +1,81 @@
 'use client'
-import {useEffect, useState} from "react";
+import {useEffect, useState, useRef} from "react";
+
+const adjectives: string[] = ["Cool", "Happy", "Fast", "Bright", "Silent", "Wild"];
+const animals: string[] = ["Tiger", "Eagle", "Shark", "Panda", "Wolf", "Falcon"];
+
+interface RegisterMessage {
+    type: "register";
+    source: string;
+    payload: RegisterMessagePayload
+}
+
+interface RegisterMessagePayload {
+    peerId: string;
+    displayName: string;
+}
+
+// Generates a random name from a selection of words
+function generatePeerName(): string {
+    const adj = adjectives[Math.floor(Math.random() * adjectives.length)];
+    const animal = adjectives[Math.floor(Math.random() * adjectives.length)];
+    return `${adj}@${animal}`;
+}
+
+// Created random id the peer can register with to the server
+// ID range 0 - 10000
+function generatePeerId(): string {
+    return "peer-" + Math.floor(Math.random() * 10000).toString();
+}
 
 export default function WebSocketComponent() {
-    // State to know if my socket is connected to the "auth" server or not
-    const [connected, setConnected] = useState(false);
+    // State to know if the socket is connected to the "auth" server or not
+    const [connected, setConnected] = useState<boolean>(false);
+    // State to track the name of this client
+    const [peerName, setPeerName] = useState<string | null>(null);
+    // State to track the ID of the peer
+    const [peerId, setPeerId] = useState<string | null>(null);
+
+    const socketRef = useRef<WebSocket | null>(null);
+
     useEffect(() => {
         const socket = new WebSocket("ws://localhost:8080/ws");
+        socketRef.current = socket;
+
         socket.onopen = (event) => {
             console.log("Connected to Auth Server");
             setConnected(true);
+
+            const newPeerName = generatePeerName();
+            setPeerName(newPeerName);
+            console.log("Generated PeerName", newPeerName);
+
+            const peerId: string = generatePeerId();
+            setPeerId(peerId);
+
+            const messagePayload: RegisterMessagePayload = {
+                peerId: peerId,
+                displayName: newPeerName,
+            }
+
+            const registerMessage: RegisterMessage = {
+                type: "register",
+                source: newPeerName,
+                payload: messagePayload,
+            };
+
+            socket.send(JSON.stringify(registerMessage));
         }
 
         // Behavior of page for when the socket connection is closed.
         socket.onclose = () => {
             console.log("Disconnected from Auth Server");
             setConnected(false);
+        }
+
+        return () => {
+            socket.close();
+            console.log("Disconnected from Auth Server via cleanup");
         }
 
     }, []);
